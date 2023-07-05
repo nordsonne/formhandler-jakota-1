ab096fe8971450197e158e709d75c808c5ca7ae2
Thomas Lüder <t.lueder@nets.de>
2023-07-05 18:40:59 +0200

    Set License file header

--- Classes/ViewHelpers/FieldNameFromPathViewHelper.php
+++ Classes/ViewHelpers/FieldNameFromPathViewHelper.php
@@ -1,8 +1,11 @@
 <?php
 
 /*
- * This file belongs to the package "TYPO3 Fluid".
- * See LICENSE.txt that was shipped with this package.
+ * This file is part of TYPO3 CMS-based extension "Formhandler" by JAKOTA.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  */
 
 namespace Typoheads\Formhandler\ViewHelpers;
